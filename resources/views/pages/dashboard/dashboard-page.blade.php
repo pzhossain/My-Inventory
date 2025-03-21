dd72bfd47dd439103d07df61cc6f705786370d78
Parvez <pzhossain@gmail.com>
2025-03-21 18:07:59 +0600

    adding Dashboard & category Page

---
+++ resources/views/pages/dashboard/dashboard-page.blade.php
@@ -0,0 +1,8 @@
+@extends('layouts.sidenav_layout');
+
+@section('content')
+
+@include('components.dashboard.summary')
+
+
+@endsection
--- routes/web.php
+++ routes/web.php
@@ -1,6 +1,12 @@
 <?php
 
-use App\Http\Controllers\HomeController;
 use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\HomeController;
+use App\Http\Controllers\CategoryController;
+use App\Http\Controllers\DashboardController;
 
 Route:: get('/', [HomeController::class, 'homePage']);
+
+
+Route:: get('/dashboard', [DashboardController::class, 'dashboardPage']);
+Route:: get('/category', [CategoryController::class, 'categoryPage']);
